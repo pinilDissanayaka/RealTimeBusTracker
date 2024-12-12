5d8400097a10aa04618a694de6e196335daac12a
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-12 18:32:56 +0530

    add map

--- server/static/map.js
+++ server/static/map.js
@@ -18,6 +18,8 @@ map.on('click', onMapClick);
 
 
 const checkpoints = new Map();
+let isFiltered = false;
+let currentCheckpoints=[];
 
 var source = new EventSource('http://127.0.0.1:5001/');
 source.addEventListener('message', function (e) {
@@ -32,18 +34,50 @@ source.addEventListener('message', function (e) {
             map.removeLayer(existingMarker);
         }
 
+        
         // Update the map with the new coordinates
         const newMarker = L.marker([obj.lon, obj.lat]).addTo(map);
         newMarker.bindPopup("Route Number :" + obj.bus_id + "").openPopup();
+        currentCheckpoints.push(newMarker);
 
         // Update the buses map with the new marker
         checkpoints.set(obj.bus_id, newMarker);
+
+        currentCheckpoints.push({
+            "bus_id": obj.bus_id,
+            "marker": newMarker
+        });
     } else {
         // Add a new marker for the bus
         const marker = L.marker([obj.lon, obj.lat]).addTo(map);
         marker.bindPopup("Route Number :" + obj.bus_id + "").openPopup();
         checkpoints.set(obj.bus_id, marker);
+        currentCheckpoints.push({
+            "bus_id": obj.bus_id,
+            "marker": marker
+        });
     }
 }, false);
+
+
+function filter(){
+    const filter_id = document.getElementById("filter_id").value.trim();
+
+    if (filter_id && isFiltered) {
+        isFiltered = true;
+        currentCheckpoints.forEach((currentCheckpoint) => {
+            if (currentCheckpoint.bus_id == filter_id) {
+                currentCheckpoint.marker.addTo(map);
+            }
+            else {
+                map.removeLayer(currentCheckpoint.marker);
+            }
+            
+        });
+    }
+    else{
+        isFiltered=false;
+    }
+}
 
 
