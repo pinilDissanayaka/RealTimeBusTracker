31ca778315dd0996ed30dd5270b405ebc1be5e8c
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-13 10:50:50 +0530

    add map filter

--- server/static/map.js
+++ server/static/map.js
@@ -1,3 +1,5 @@
+const BASE_URL ="http://127.0.0.1:5001"
+
 var popup = L.popup();
 
 var map = L.map('map').setView([6.930243972119172, 79.87848236150131], 10);
@@ -17,9 +19,13 @@ function onMapClick(e) {
 map.on('click', onMapClick);
 
 
-const checkpoints = new Map();
 
-var source = new EventSource('http://127.0.0.1:5001/');
+
+const checkpoints = new Map();
+const current_chechpoint=new Map();
+const isFiltered =false;
+
+var source = new EventSource(BASE_URL + "/");
 source.addEventListener('message', function (e) {
     const obj = JSON.parse(e.data);
 
@@ -40,15 +46,32 @@ source.addEventListener('message', function (e) {
 
         // Update the buses map with the new marker
         checkpoints.set(obj.bus_id, newMarker);
+        current_chechpoint.set(obj.bus_id, newMarker);
 
     } else {
         // Add a new marker for the bus
         const marker = L.marker([obj.lon, obj.lat]).addTo(map);
         marker.bindPopup("Route Number :" + obj.bus_id + "").openPopup();
         checkpoints.set(obj.bus_id, marker);
+        current_chechpoint.set(obj.bus_id, marker);
     }
 }, false);
 
 
 
+function filterMap(){
+    var filter_button=document.getElementById("filter-button");
 
+    if(filter_button){
+        var filter_query=filter_button.value.trim();
+        console.log(filter_query);
+    }
+    else{
+        filter_button.textContent="Filter";
+    }
+
+    
+
+    
+
+}
